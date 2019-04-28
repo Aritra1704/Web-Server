f53e077673b5abe1fd08ed7a0ef42cf63afffe3b
Aritra1704 <aritra1704@gmail.com>
2019-04-28 14:32:17 +0530

    Starting dynamic templates

--- src/app.js
+++ src/app.js
@@ -7,12 +7,30 @@ console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
 app.use(express.static(publicDirectoryPath))
 
 app.set('view engine', 'hbs')
 
 app.get('', (req, res)=> {
-    res.render('index')
+    res.render('index', {
+        title: 'Weather',
+        name: 'Aritra'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Aritra'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        helpText: 'This is some helpful text'
+    })
 })
 
 app.get('/weather', (req, res) => {
@@ -37,14 +55,7 @@ app.listen(3000, () => {
 //     res.send('<h1>Weather</h1>')
 // })
 
-// app.get('/help', (req, res) => {
-//     res.send({
-//         name: 'Aritra',
-//         age: 30
-//     })
-// })
 
-// app.get('/about', (req, res) => {
-//     res.send('<h1>About title</h1>')
-// })
+
+
 
